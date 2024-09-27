03c7d5b749333cf46d7583e92a5cf8c224d9c243
ljy0221 <jaeyeong221@gmail.com>
2024-09-27 14:20:43 +0900

    second's revised code

--- operation.py
+++ operation.py
@@ -1,14 +1,17 @@
 def add(num1, num2):
     return num1 + num2
 
-
 def subtract(num1, num2):
     return num1 - num2
 
-
 def multiply(num1, num2):
-    return 0
-
+    if num1 == 0 or num2 == 0
+        return 0
+    else:
+        return num1 * num2
 
 def divide(num1, num2):
-    return 0
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Cannot divide by zero"
